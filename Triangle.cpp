f209552394c54cc9ab616af0b65bc6398b325c44
LevSvl8 <svetlakov_l@bk.ru>
2022-06-15 12:19:38 +0300

    first commit

---
+++ Triangle.cpp
@@ -0,0 +1,23 @@
+class Car
+{
+    int dvigatel;
+    int max_speed;
+}
+
+class Audi: Car{
+    char model[10]}
+
+class A4: Audi{
+    char Komplectation[10]}
+
+class A4_comfort: A4{
+    bool ABC}
+
+class A4_premium: A4{
+    bool kruiz_kontrol}
+
+int main()
+{
+    
+    A A4(340,260,A4,comf,1);
+}
---
+++ Классы.cpp
@@ -0,0 +1,18 @@
+
+
+class Progr_lang
+{
+
+}
+class Logicheskie: Progr_lang
+{
+
+}
+class funcsionalnye: Progr_lang 
+{
+
+}
+class OOP: funcsionalnye
+{
+
+}
